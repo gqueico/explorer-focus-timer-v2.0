ae5a0a1ce752e25b99e505ea98c066550d22d3d4
gqueico <gqueico@gmail.com>
2024-03-10 23:13:19 -0300

    :hammer: Create Toggle button accessibility event

--- src/toggle-mode.js
+++ src/toggle-mode.js
@@ -3,4 +3,9 @@ const buttonToggle = document.getElementById('toggle-mode')
 
 buttonToggle.addEventListener('click', event => {
   document.documentElement.classList.toggle('dark')
+
+  const mode = lightMode ? 'Dark' : 'Light'
+  event.currentTarget.querySelector('span').textContent = `${mode} mode ativado!`
+
+  lightMode = !lightMode
 })
