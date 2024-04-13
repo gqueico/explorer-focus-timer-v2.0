0652e1907cb9e749f24aacb8982698b176c434f8
gqueico <gqueico@gmail.com>
2024-04-12 23:27:51 -0300

    :construction:  Creating functions that register button events

--- src/FocusTimer/events.js
+++ src/FocusTimer/events.js
@@ -1,3 +1,11 @@
+import { controls } from './elements.js';
+
 export function registerControls() {
-  
+  controls.addEventListener('click', event => {
+    console.log(event.target)
+  })
+
+  // sounds.addEventListener('click', event => {
+  //   console.log(event.target)
+  // })
 }
