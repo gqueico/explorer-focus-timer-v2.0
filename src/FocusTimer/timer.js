1652d3aba2e759c0d5b1b16edb55e76eb040d31b
gqueico <gqueico@gmail.com>
2024-04-17 08:05:45 -0300

    :sparkles: Create countdown structure

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -1,10 +1,20 @@
 import state from './state.js'
-import * as elements from './elements.js'
+import * as element from './elements.js'
+
+export function countdown() {
+  if(!state.isRunning) {
+    return
+  }
+  
+  console.log('Inicia Timer')
+
+  setTimeout(() => countdown(), 1000)
+}
 
 export function updateDisplay(minutes, seconds) {
   minutes = minutes ?? state.minutes
   seconds = seconds ?? state.seconds
 
-  elements.minutes.textContent = String(minutes).padStart(2, '0')
-  elements.seconds.textContent = String(seconds).padStart(2, '0')
+  element.minutes.textContent = String(minutes).padStart(2, '0')
+  element.seconds.textContent = String(seconds).padStart(2, '0')
 }
