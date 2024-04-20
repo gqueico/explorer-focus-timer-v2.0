008879d00c1e836197fca319866dc868a74f6194
gqueico <gqueico@gmail.com>
2024-04-20 04:52:36 -0300

    :sparkles: Create increaseTime function

--- src/FocusTimer/actions.js
+++ src/FocusTimer/actions.js
@@ -1,5 +1,6 @@
 import state from './state.js'
 import * as timer from './timer.js'
+import * as element from './elements.js'
 
 export function toggleRunning() {
   document.documentElement.classList.add('running')
@@ -11,4 +12,16 @@ export function toggleRunning() {
 export function stop() {
   document.documentElement.classList.remove('running')
   state.isRunning = false
+}
+
+export function increaseTime() {
+  let minutes = Number(element.minutes.textContent)
+
+  minutes+= 5
+
+  if(minutes > 60) {
+    return
+  }
+  
+  timer.updateDisplay(minutes)
 }
