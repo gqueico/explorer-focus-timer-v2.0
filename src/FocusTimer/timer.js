a2739f351db4c9c67d98db7875145fc22020b287
gqueico <gqueico@gmail.com>
2024-04-26 23:32:35 -0300

    :sparkles: Correcting Timeout accumulation

--- src/FocusTimer/timer.js
+++ src/FocusTimer/timer.js
@@ -3,6 +3,8 @@ import * as element from './elements.js'
 import { stop } from './actions.js'
 
 export function countdown() {
+  clearTimeout(state.countdownId)
+
   if(!state.isRunning) {
     return
   }
@@ -24,7 +26,7 @@ export function countdown() {
   }
 
   updateDisplay(minutes, seconds)
-  setTimeout(() => countdown(), 1000)
+  state.countdownId = setTimeout(() => countdown(), 1000)
 }
 
 export function updateDisplay(minutes, seconds) {
