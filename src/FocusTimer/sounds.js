9ba5992410d7f86d42c98d020a40799a0cc0bc98
gqueico <gqueico@gmail.com>
2024-05-23 05:13:53 -0300

    :sparkles: Sets audio from the sounds section to loop

--- src/FocusTimer/sounds.js
+++ src/FocusTimer/sounds.js
@@ -5,3 +5,8 @@ export const forest = new Audio('./assets/audio/forest.wav')
 export const rain = new Audio('./assets/audio/rain.wav')
 export const coffeeShop = new Audio('./assets/audio/coffee-shop.wav')
 export const fireplace = new Audio('./assets/audio/fireplace.wav')
+
+forest.loop = true
+rain.loop = true
+coffeeShop.loop = true
+fireplace.loop = true
